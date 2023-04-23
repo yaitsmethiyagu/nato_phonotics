2cb5e2aff75e7c37229db7f0a878dc5b8f4c3354
Thiyagarajan Shankar <yaitsmethiyagu@gmail.com>
2023-04-23 21:52:41 +0530

    initial commit

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import pandas
 
 df = pandas.read_csv('nato_phonetic_alphabet.csv')
 
-dic_df = {row.letter: row.code for (index, row) in df.iterrows()}
+dictionary_df = {row.letter: row.code for (index, row) in df.iterrows()}
 
 user_input = input("Type your words for Nato Phonetic conversion \n")
 
 user_input = [letters.upper() for letters in user_input]
 
-output = [dic_df[letters] for letters in user_input]
+output = [dictionary_df[letters] for letters in user_input]
 
 print(output)
